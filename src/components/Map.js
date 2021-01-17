92102ba1ec0d2e9b6f5c3f497b701e8506ed4198
Phillbert Tan <philly@phillytan.xyz>
2021-01-16 22:46:50 -0800

    Bug Fixes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,68 +1,58 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './Map.css';
 
 const handleApiLoaded = (map, maps, establishments) => {
-    let heatmapData = [];
-    // for (let i = 0; i < 250; i++) {
-    //     const max = 0.01;
-    //     const min = -max;
-    //     let x = Math.random() * (max - min) + min;
-    //     let y = Math.random() * (max - min) + min;
-    //     heatmapData.push({ location: new maps.LatLng(49.265 + x, -123.250 + y), weight: 0.1 });
-    // }
-
-    if (establishments.length > 0) {
-        console.log(establishments);
-
-        heatmapData = establishments.map((establishment) => {
-            let lat = establishment.lat;
-            let lng = establishment.lng;
-
-            const weight = establishment.count / 10 ;
-
-            return { location: new maps.LatLng(lat, lng), weight: weight }
-        })
-    }
-
-    console.log(heatmapData)
-
-    let heatmap = new maps.visualization.HeatmapLayer({
-        data: heatmapData
-    });
-
-    heatmap.set("radius", heatmap.get("radius") ? null : 20);
-
-    heatmap.setMap(map);
+  let heatmapData = [];
+  if (establishments.length > 0) {
+      console.log(establishments);
+      heatmapData = establishments.map((establishment) => {
+          let lat = establishment.lat;
+          let lng = establishment.lng;
+          const weight = establishment.count / 10 ;
+          return { location: new maps.LatLng(lat, lng), weight: weight }
+      })
+  }
+  let heatmap = new maps.visualization.HeatmapLayer({
+      data: heatmapData
+  });
+  heatmap.set("radius", heatmap.get("radius") ? null : 20);
+  heatmap.setMap(map);
 };
 
+const Map = ({ establishments }) => {
 
-class Map extends Component {
+  const [ loaded, setLoaded ] = React.useState(false)
 
+  const [ map, setMap ] = React.useState()
+  const [ maps, setMaps ] = React.useState()
 
-
-    static defaultProps = {
-        center: {
-            lat: 49.2656534435675,
-            lng: -123.25035467904151
-        },
-        zoom: 13
-    };
-
-    render() {
-        return (
-            <div className="mapContainer">
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyB12Z1ngare18mezYXsvcfjInBIhbmdu-Y' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                    yesIWantToUseGoogleMapApiInternals
-                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps, this.props.establishments)}
-                >
-                </GoogleMapReact>
-            </div>
-        );
+  useEffect(() => {
+    if (loaded) {
+      handleApiLoaded(map, maps, establishments)
     }
+  }, [establishments, loaded, map, maps])
+
+  return (
+    <div className="mapContainer">
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyB12Z1ngare18mezYXsvcfjInBIhbmdu-Y' }}
+            defaultCenter={{
+              lat: 49.2656534435675,
+              lng: -123.25035467904151
+            }}
+            defaultZoom={13}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => {
+              setMap(map)
+              setMaps(maps)
+              // handleApiLoaded(map, maps, this.props.establishments)
+              setLoaded(true)
+            }}
+        >
+        </GoogleMapReact>
+    </div>
+  )
 }
 
 export default Map;
