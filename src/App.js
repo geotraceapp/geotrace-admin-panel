c47a7baeea28d36787f6586944c90137772a3b66
Phillbert Tan <philly@phillytan.xyz>
2021-01-16 21:33:14 -0800

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -5,37 +5,10 @@ import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 import React from 'react';
 import firebase from 'firebase';
 
-// const FirestoreCollection = () => {
-//   const { error, loading, value } = useCollection(
-//     firebase.firestore().collection('hooks')
-//   );
-//   return (
-//     <div>
-//       <p>
-//         {error && <strong>Error: {error}</strong>}
-//         {loading && <span>Collection: Loading...</span>}
-//         {value && (
-//           <span>
-//             Collection:{' '}
-//             {value.docs.map(doc => (
-//               <React.Fragment key={doc.id}>
-//                 {JSON.stringify(doc.data())},{' '}
-//               </React.Fragment>
-//             ))}
-//           </span>
-//         )}
-//       </p>
-//     </div>
-//   );
-// };
-
-
-
-
 function App() {
 
-  const { error, loading, value } = useCollectionDataOnce(firebase.firestore().collection('establishments'))
-  console.log(firebase, error, loading, value);
+  const [values, loading] = useCollectionDataOnce(firebase.firestore().collection('establishments'))
+  console.log(loading, values);
 
   return (
     <div className="App">
